b057ea27b6e0704ec82e7c11d16685fbaa5b06d0
suwoni <topspin1278@naver.com>
2021-01-15 10:33:46 +0900

    feat - application log 설정 (#3)
    * Merge branch 'master' of https://github.com/cionman/infra-subway-properties
    
    * feat - application log 설정

--- src/main/java/nextstep/subway/station/ui/StationController.java
+++ src/main/java/nextstep/subway/station/ui/StationController.java
@@ -1,8 +1,11 @@
 package nextstep.subway.station.ui;
 
+import nextstep.subway.line.ui.LineController;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.dto.StationRequest;
 import nextstep.subway.station.dto.StationResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,7 @@ import java.util.List;
 
 @RestController
 public class StationController {
+    private static final Logger log = LoggerFactory.getLogger(StationController.class);
     private StationService stationService;
 
     public StationController(StationService stationService) {
@@ -21,6 +25,7 @@ public class StationController {
 
     @PostMapping("/stations")
     public ResponseEntity<StationResponse> createStation(@RequestBody StationRequest stationRequest) {
+        log.debug("Station create param :{}", stationRequest);
         StationResponse station = stationService.saveStation(stationRequest);
         return ResponseEntity.created(URI.create("/stations/" + station.getId())).body(station);
     }
